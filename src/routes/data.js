4626639b3feff2ec0f72cc675ce3376728fce226
Al Varrel Putra Kusuma <varrelksm@gmail.com>
2018-11-30 13:53:08 +0700

    Put JSON ok

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var secured = require('../lib/middleware/secured');
 var router = express.Router();
+var spotify = require('../data/spotify.json');
 
 /* GET user profile. */
-router.get('/spotify', secured(), function (req, res, next) {
+/* router.get('/spotify', secured(), function (req, res, next) {
   const { _raw, _json, ...userProfile } = req.user;
   res.render('spotify', {
     userProfile: JSON.stringify(userProfile, null, 2),
     title: 'Profile page'
   });
+}); */
+
+router.get('/spotify', secured(), function (req, res, next) {
+  var json = spotify;
+  res.send(spotify);
 });
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Auth0 Webapp sample Nodejs' });
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
