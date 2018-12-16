fadbe9983837ba8cf860cdc1d0fdefa1b2ff0fef
Al Varrel Putra Kusuma <varrelksm@gmail.com>
2018-12-16 19:14:19 +0700

    delete local

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback' || 'https://musicgo.herokuapp.com/callback'
+      process.env.AUTH0_CALLBACK_URL || 'https://musicgo.herokuapp.com/callback'
                                       || 'http://musicgo.herokuapp.com/callback'
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
