e106a4a6e913864e56bccb27df2d3154cc50a0e8
Al Varrel Putra Kusuma <varrelksm@gmail.com>
2018-12-16 16:53:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ var strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback' || 'https://musicgo.herokuapp.com/callback'
+                                      || 'http://musicgo.herokuapp.com/callback'
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
