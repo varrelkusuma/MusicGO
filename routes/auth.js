a99c94a1b12d5cf5ee9ffa1664aaacf84ba8f8fc
Al Varrel Putra Kusuma <varrelksm@gmail.com>
2018-12-16 18:37:47 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,15 +6,14 @@ var passport = require('passport');
 router.get('/login', passport.authenticate('auth0', {
   scope: 'openid email profile'
 }), function (req, res) {
-  console.log('woy');
-  //res.redirect('/');
+  res.redirect('/');
 });
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
     if (err) { return next(err); }
-    if (!user) { return res.redirect('/login'); }
+    if (!user) { return res.redirect('/spotify'); }
     req.logIn(user, function (err) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
